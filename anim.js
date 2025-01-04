cb76ce29cf8977a84ef9fbc1a2fc113e01266fda
juarezrildo <86016042+juarezrildo@users.noreply.github.com>
2025-01-04 15:22:06 -0400

    Add files via upload

---
+++ anim.js
@@ -0,0 +1,4 @@
+// Sincronizar las letras con la canción
+var audio = document.querySelector("audio");
+var lyrics = document.querySelector("#lyrics");
+//Actualizacion, no pille los subtitulos en sincro con la cancion, asi que va solo cancion
